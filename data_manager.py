aa5e6a1943f852a4214923960170c7c51806a56f
Getulus <tamasgaj@gmail.com>
2020-03-30 18:05:35 +0200

    question

--- data_manager.py
+++ data_manager.py
@@ -1,20 +1,28 @@
 import csv
 
 
+
+
 def read_csv(filename):
     file_list = []
     with open(filename, "r") as file:
         csv_reader = csv.reader(file, delimiter=',')
+        next(csv_reader)
         for row in csv_reader:
             file_list.append(row)
-    file_list = file_list.reverse()
-    return file_list
+    return file_list[::-1]
+
+ALL_QUESTION_DATAS = read_csv('sample_data/question.csv')
+ALL_ANSWER_DATAS = read_csv('sample_data/answer.csv')
 
 
 
 
-#def main():
-#    read_csv('sample_data/question.csv')
 
-#if __name__ == '__main__':
-#    main()
+"""
+def main():
+    print(read_csv('sample_data/question.csv'))
+
+if __name__ == '__main__':
+    main()
+"""
--- server.py
+++ server.py
@@ -6,12 +6,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+
+    return render_template("main.html")
 
 @app.route('/list')
 def list():
-    question_list = data_manager.read_csv('sample_data/question.csv')
-    return render_template('list.html', question_list=question_list)
+
+    question_list = []
+    for rows in data_manager.ALL_QUESTION_DATAS:
+        question_list.append(rows[5])
+
+    return render_template('list.html', question_list=question_list, rows=len(question_list), cols=len(question_list[0]), id=id)
+
+@app.route("/list/<question_id>")
+def display_question(question_id):
+    return render_template("display_question.html")
+
+
 
 
 if __name__ == "__main__":
