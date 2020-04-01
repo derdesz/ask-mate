131e39453782680f7953000a8e3167011ed61903
Getulus <tamasgaj@gmail.com>
2020-04-01 20:14:33 +0200

    szar az egész

--- data_manager.py
+++ data_manager.py
@@ -2,36 +2,71 @@ import csv
 import random
 
 
-def create_id():
+def create_id(id_type):
     id = 0
-    while id not in ALL_ID:
+    while id not in id_type:
         id = str(random.randint(1, 100))
-        if id not in ALL_ID:
-            ALL_ID.append(id)
+        if id not in id_type:
+            id_type.append(id)
             return id
 
 
 
+def write_csv(filename, datas):
+
+    with open(filename, "w") as w_file:
+        writer = csv.DictWriter(w_file, fieldnames=HEADERS)
+        writer.writeheader()
+
+        for i in range(len(datas)):
+            writer.writerows(HEADERS)
+
+
+
 def read_csv(filename):
-    file_list = []
-    with open(filename, "r") as file:
-        csv_reader = csv.reader(file, delimiter=',')
-        next(csv_reader)
-        for row in csv_reader:
-            file_list.append(row)
-    return file_list[::-1]
+    all_data = []
+    with open (filename, "r") as file:
+        csv_reader = csv.DictReader(file)
+        for rows in csv_reader:
+            all_data.append(rows)
+        return all_data
+
+
+def add_element(filename, datas):
+    with open(filename, "a") as file:
+        row = ",".join(datas)
+        file.write(row + "\n")
+
+
+
+
 
 ALL_QUESTION_DATAS = read_csv('sample_data/question.csv')
 ALL_ANSWER_DATAS = read_csv('sample_data/answer.csv')
-ALL_ID = ["1","2","3"]
+ALL_Q_ID = ["1","2","3"]
+ALL_A_ID = ["1", "2", "3"]
+HEADERS = ALL_QUESTION_DATAS[0].keys()
 
 
 
 
-"""
+
+
 def main():
-    print(read_csv('sample_data/question.csv'))
 
+
+    print(ALL_QUESTION_DATAS)
+    print("\n\n")
+    print(ALL_QUESTION_DATAS[1])
+    print("\n\n")
+    print(ALL_QUESTION_DATAS[0]["id"])
+    print("\n\n")
+    for head in HEADERS:
+        print(head)
+
+    print(write_csv('sample_data/question.csv', ALL_QUESTION_DATAS))
+
+
+        
 if __name__ == '__main__':
     main()
-"""
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import csv
 import data_manager
+import time
 app = Flask(__name__)
 
 
@@ -11,65 +12,99 @@ def hello():
 
 @app.route('/list')
 def list():
-    return render_template('list.html', rows=len(data_manager.ALL_QUESTION_DATAS), all_data=data_manager.ALL_QUESTION_DATAS)
+    all_question_datas = data_manager.read_csv('sample_data/question.csv')
+    return render_template('list.html', rows=len(all_question_datas), all_data=all_question_datas)
+
+
+
 
 @app.route("/list/question/<string:question_id>")
 def display_question(question_id):
-    all_q_data = data_manager.ALL_QUESTION_DATAS
-    all_a_data = data_manager.ALL_ANSWER_DATAS
+    all_q_data = data_manager.read_csv('sample_data/question.csv')
+    all_a_data = data_manager.read_csv('sample_data/answer.csv')
+    current_a_data = []
     for i in range(len(all_q_data)):
-        if question_id == all_q_data[i][0]:
+        if question_id == all_q_data[i]["id"]:
             current_q_data = all_q_data[i]
 
     for i in range(len(all_a_data)):
-        if question_id == all_a_data[i][0]:
-            current_a_data = all_a_data[i]
+        if question_id == all_a_data[i]["question_id"]:
+            current_a_data.append(all_a_data[i])
 
 
 
     return render_template("display_question.html", id=question_id, current_a_data=current_a_data, current_q_data=current_q_data,
                            q_rows=len(data_manager.ALL_QUESTION_DATAS),
-                           a_rows=len(data_manager.ALL_ANSWER_DATAS), a_col=(len(current_a_data) - 1))
+                           a_rows=len(data_manager.ALL_ANSWER_DATAS))
 
 
 @app.route("/list/add-question", methods=["POST", "GET"])
 def ask_question():
-    q_id = data_manager.create_id()
 
+    time_stample = str(time.time())
+    q_id = data_manager.create_id(data_manager.ALL_Q_ID)
     if request.method == "POST":
-        datas = [request.form["title"], request.form["message"]]
-        data_manager.ALL_QUESTION_DATAS.append([q_id, "", "", "", datas[0], datas[1], ""])
-        data_manager.ALL_ANSWER_DATAS.append([q_id, "", "", "", "", "51340"])
+        current_q_data = [q_id, time_stample, '0', '0', request.form["title"], request.form["message"], ' ']
+        data_manager.add_element('sample_data/question.csv', current_q_data)
+
         return redirect(url_for("display_question", question_id=q_id))
+
     else:
         return render_template("add_question.html")
+
+
 
 
 @app.route("/question/<question_id>/new-answer", methods=["POST", "GET"])
 def new_answer(question_id):
     if request.method == "POST":
-        all_asw = data_manager.ALL_ANSWER_DATAS
-        for i in range(len(all_asw)):
-            if question_id == all_asw[i][0]:
-                data_manager.ALL_ANSWER_DATAS[i].insert(4, request.form["message"])
+        time_stample = str(time.time())
+        a_id = data_manager.create_id(data_manager.ALL_A_ID)
+
+        answer = [a_id, time_stample, "0", question_id, request.form["message"], " "]
+        data_manager.add_element("sample_data/answer.csv", answer)
         return redirect(url_for("display_question", question_id=question_id))
+
     else:
         return render_template("new_answer.html")
 
 
 @app.route("/question/<question_id>/edit", methods=["POST","GET"])
 def edit_question(question_id):
-    all_q_data = data_manager.ALL_QUESTION_DATAS
+    all_q_data = data_manager.read_csv('sample_data/question.csv')
     for i in range(len(all_q_data)):
-        if question_id == all_q_data[i][0]:
+        if question_id == all_q_data[i]["id"]:
             current_data = all_q_data[i]
+            index = i
 
     if request.method == "POST":
-        current_data[4] = request.form["title"]
-        current_data[5] = request.form["message"]
+        all_q_data.remove(all_q_data[index])
+        time_stample = str(time.time())
+        q_id = data_manager.create_id(data_manager.ALL_Q_ID)
+        question = [q_id, time_stample, "0", "0", request.form["title"], request.form["message"], " "]
+        data_manager.add_element("sample_data/answer.csv", question)
         return redirect(url_for("display_question", question_id=question_id))
     else:
         return render_template("edit.html", current_data=current_data)
+        
+
+@app.route("/question/<question_id>/delete")
+def delete_question(question_id):
+    all_q_data = data_manager.read_csv('sample_data/question.csv')
+    all_a_data = data_manager.read_csv('sample_data/answer.csv')
+    for i in range(len(all_q_data)):
+        if question_id == all_q_data[i][0]:
+            data_manager.ALL_QUESTION_DATAS.remove(all_q_data[i])
+
+    for i in range(len(all_a_data)):
+        if question_id == all_a_data[i][0]:
+            data_manager.ALL_ANSWER_DATAS.remove(all_a_data[i])
+
+    data_manager.ALL_ID.remove(question_id)
+
+    return redirect(url_for("list"))        
+        
+        
 
 '''
 @app.route("/answer/<answer_id>/delete")
@@ -86,23 +121,8 @@ def delete_(question_id):
 
 
 
-@app.route("/question/<question_id>/delete")
-def delete_question(question_id):
-    all_q_data = data_manager.ALL_QUESTION_DATAS
-    all_a_data = data_manager.ALL_ANSWER_DATAS
-    for i in range(len(all_q_data)):
-        if question_id == all_q_data[i][0]:
-            data_manager.ALL_QUESTION_DATAS.remove(all_q_data[i])
 
-    for i in range(len(all_a_data)):
-        if question_id == all_a_data[i][0]:
-            data_manager.ALL_ANSWER_DATAS.remove(all_a_data[i])
-
-    data_manager.ALL_ID.remove(question_id)
-
-    return redirect(url_for("list"))
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-id,submission_time,vote_number,question_id,message,image
